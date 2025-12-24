7e74904c8be4b024cb255caa83d7f1337f562a1d
developerMasum <developer.masum2001@gmail.com>
2025-12-24 10:11:51 +0600

    added variables

--- Day-1/variables.py
+++ Day-1/variables.py
@@ -17,3 +17,56 @@ x = "thirty"
 X  = "Crow"
 print (x)
 print (X)
+
+# camel case 
+myVariableName = "John"
+print (myVariableName)
+
+# snake case
+mu_variable_name = "Doe"
+print (mu_variable_name)
+
+# Pascal case
+MyVariableName = "Smith"
+print (MyVariableName)
+
+
+
+#  Multiple Variables Multiple Variables
+
+a, b, c = 5, "Hello", 7.5
+print (a)
+print (b)
+print (c)
+
+
+x=y=z= "Same Value"
+print (x)
+print (y)
+print (z)
+
+# Unpacking a Collection
+techs = ["Python", "Java", "C++"]
+a, b, c = techs
+print (a)
+print (b)
+print (c)
+
+
+# Output Variables
+x = "awesome" 
+y = "Python"
+z= "journey"
+print (x, y, z)
+print (x + " " +  "looks" + y + " " + z)
+
+a = 4
+b = 5
+print(a + b)
+
+first_name = "John"
+age = 36
+str_age = "30"
+#  print(first_name + age)
+print(first_name + " is " + str(age) + " years old.")
+print(first_name + " is " + str(int(str_age)) + " years old.")
