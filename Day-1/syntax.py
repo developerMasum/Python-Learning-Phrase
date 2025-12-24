711e9c4e92349492b6ee63ac988eac953bceeef7
developerMasum <developer.masum2001@gmail.com>
2025-12-24 09:50:40 +0600

    Python Learning Phrase

---
+++ Day-1/syntax.py
@@ -0,0 +1,9 @@
+if 3>2 :
+    print("3 is greater than 2")
+
+
+    x= 10
+    y = "hey bro ,This is my first python code"
+
+    print(x)  
+    print(y)
---
+++ Day-1/variables.py
@@ -0,0 +1,19 @@
+print ("I'm learning Python from", 24, "December", 2025)
+
+x= 30
+y = "mr X"
+z = 3.5
+print (x)
+print (y)
+print (z)
+
+# lets find out the types 
+print (type(x))
+print (type(y)) 
+
+# Overwrite issues
+
+x = "thirty"
+X  = "Crow"
+print (x)
+print (X)
