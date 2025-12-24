3b019021355765efd18e4d624f17bc9caaf50f64
developerMasum <developer.masum2001@gmail.com>
2025-12-24 11:02:21 +0600

    test completed

---
+++ Day-1/variable-exam.py
@@ -0,0 +1,79 @@
+# --------------------------------------------------------------
+# Test results:
+
+# ⭐ Overall Feedback
+
+# 🔥 Score: 9/10
+# ..................... by chatgpt mock questions .....................
+
+
+# Problem:1
+x= 24
+print("My age is", x)
+
+# Problem:2
+a = 12
+b =  5
+sum = a + b
+difference = a - b
+product = a * b
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+
+# Problem:3
+x = 10
+y = 20
+x, y = y, x
+print("After swapping: x =", x, ", y =", y)
+
+# Problem:4
+first_name = "Ratul"
+last_name = "Islam"
+print("Full Name:", first_name + " " + last_name)
+
+# Problem:5
+n = 40
+p = 10
+print("Total cost :", (3*n)+(5*p))
+
+# Problem:6
+celsius = 25
+F = (celsius * 9/5) + 32
+print("Temperature in Fahrenheit:", F)
+
+# Problem:7
+math = 23
+science = 17
+english = 20
+
+print("Total Marks:", math + science + english)
+print("Average Marks:", (math + science + english) / 3)
+
+
+# Problem:8
+int_num = 7
+float_num = 3.5
+string_val = "Hello"
+
+print("Integer:", type(int_num))
+print("Float:", type(float_num))
+print("String:", type(string_val))
+
+# Problem:9
+item_name = "Notebook"
+item_price = 15.75
+item_quantity = 3
+
+print("Item:", item_name)
+print("Total Price:", item_price * item_quantity)
+
+
+# problem:10
+
+mins = 1000
+hours = mins / 60
+remaining = mins % 60
+print(int(mins), "minutes is", int(hours), "hours and", int(remaining), "minutes.")
+
+
